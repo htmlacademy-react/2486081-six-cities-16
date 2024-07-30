bf6ea6f7b28226e67c40a9375a58df749ddfaa74
Roman <morozov9786@gmail.com>
2024-07-30 17:24:29 +0300

    Создаст компонент стр. 404.

---
+++ src/pages/not-found-page/not-found-page.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(): JSX.Element {
+  return (
+    <section>
+      <h1>Такой страницы нет.</h1>
+      <a href="/">Вернуться на главную</a>
+    </section>
+  );
+}
